9b480a40c83e6de4dafb3c5c754e7730fdfd0d30
bhuwan1301 <bhuwancp1301@gmail.com>
2026-07-19 00:15:40 +0530

    Changed email again

--- backend_express/utils/sendEmail.js
+++ backend_express/utils/sendEmail.js
@@ -6,10 +6,17 @@ const sendEmail = async (email, subject, text) => {
 
         const transporter = nodemailer.createTransport({
             service: "gmail",
-            family: 4,
             auth: {
                 user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS
+                pass: process.env.EMAIL_PASS,
+            },
+        });
+
+        transporter.verify((err, success) => {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log("SMTP Ready");
             }
         });
 
