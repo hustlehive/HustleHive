49eac05cd7d8bca4e01090c3ce56263877ab4c18
hustlehive <hustleehivee@gmail.com>
2026-07-19 01:48:10 +0530

    Update server.js

--- backend_express/server.js
+++ backend_express/server.js
@@ -1,6 +1,9 @@
 const dotenv = require("dotenv");
 dotenv.config();
-console.log("Node Version:", process.version);
+
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
+
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
