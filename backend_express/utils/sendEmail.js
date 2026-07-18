a0e93a943d162aa9efaed66f1703b2c924eac9e6
bhuwan1301 <bhuwancp1301@gmail.com>
2026-07-19 00:06:50 +0530

    fixed email bug

--- backend_express/utils/sendEmail.js
+++ backend_express/utils/sendEmail.js
@@ -6,6 +6,7 @@ const sendEmail = async (email, subject, text) => {
 
         const transporter = nodemailer.createTransport({
             service: "gmail",
+            family: 4,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
