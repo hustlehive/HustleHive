f351ef52107ce54ab255736243445df47bb5f9a8
bhuwan1301 <bhuwancp1301@gmail.com>
2026-07-19 00:30:51 +0530

    Again changed email logic

--- backend_express/utils/sendEmail.js
+++ backend_express/utils/sendEmail.js
@@ -5,9 +5,7 @@ const sendEmail = async (email, subject, text) => {
     try {
 
         const transporter = nodemailer.createTransport({
-            host: "smtp.gmail.com",
-            port: 587,
-            secure: false,
+            service: "gmail",
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS,
